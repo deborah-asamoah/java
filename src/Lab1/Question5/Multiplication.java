674a017abfa2203f79091a81c5b2874aeee7256d
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-03-28 18:25:07 +0000

    Question5

---
+++ src/Lab1/Question5/Multiplication.java
@@ -0,0 +1,10 @@
+package Lab1.Question5;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        for (int i =0; i <=10; i++)
+            System.out.println(i);
+
+        
+    }
+}
