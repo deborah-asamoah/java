f3fe4eb331df40e640a405f035c6aa542260f87d
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-03-29 14:29:07 +0000

    Account class has been created

---
+++ src/ObjectsAndClasses/Account.java
@@ -0,0 +1,14 @@
+package ObjectsAndClasses;
+
+public class Account {
+    private double totalTrades;
+
+    public double getTotalTrades(){
+        return totalTrades;
+    }
+
+    public void setTotalTrades (double totalTrades){
+        this.totalTrades = totalTrades;
+    }
+
+}
