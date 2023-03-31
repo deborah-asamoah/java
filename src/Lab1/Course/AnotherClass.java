e34228cab03a9dc0065f18a07b1dd22a3688f169
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-03-31 17:24:23 +0000

    course

---
+++ src/Lab1/Course/AnotherClass.java
@@ -0,0 +1,7 @@
+package Lab1.Course;
+
+public class AnotherClass {
+    public static void display(){
+        System.out.println("Another Class display method");
+    }
+}
---
+++ src/Lab1/Course/Hello.java
@@ -0,0 +1,9 @@
+package Lab1.Course;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello WOrld");
+        AnotherClass.display();
+
+    }
+}
