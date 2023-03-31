ce36b153c7c046d15594606438f3f75f7b998adf
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-03-31 17:26:55 +0000

    lab1

--- src/Lab1/Question5/Multiplication.java
+++ src/Lab1/Question5/Multiplication.java
@@ -2,9 +2,12 @@ package Lab1.Question5;
 
 public class Multiplication {
     public static void main(String[] args) {
-        for (int i =0; i <=10; i++)
-            System.out.println(i);
-
-        
+        for (int i =0; i <=10; i++) {
+            System.out.println( "Multiplication by " + i);
+            System.out.println("********************************");
+            for (int j = 1; j <= 12; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+            }
+        }
     }
 }
